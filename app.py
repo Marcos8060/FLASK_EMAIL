7b1ec0fa1c308d41778499c0ad7da2f8bd5df8e0
Marcos8060 <marcosgav80@gmail.com>
2022-02-02 22:04:36 +0300

    created the databse for the app

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
-from flask import Flask,render_template
+from crypt import methods
+from enum import unique
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://marcos:getaways@localhost/email_messenger'
+
+db = SQLAlchemy(app)
+
+# user model
+class Data(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String,unique=True)
+    subject = db.Column(db.String(120))
+    message = db.Column(db.String(255))
+
+    def __init__(self,email,subject,message):
+        self.email = email
+        self.subject = subject
+        self.message = message
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/success')
+@app.route('/success', methods=['POST','GET'])
 def success():
+    if request.method == 'POST':
+        email = request.form['email']
+        subject = request.form['subject']
+        msg = request.form['message']
+        print(email,subject,msg)
+        data = Data(email,subject,msg)
+        db.session.add(data)
+        db.session.commit()
     message = 'Your message is sent successfully!'
     return render_template('success.html', message=message) 
 

13fd2eca48e113299ee3083289ced7c49adc899f
Marcos8060 <marcosgav80@gmail.com>
2022-02-02 20:52:13 +0300

    added the success page

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import flask
-from app import app
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 
-db = sqlalchemy(app)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/success')
+def success():
+    message = 'Your message is sent successfully!'
+    return render_template('success.html', message=message) 
+
+
+if __name__=='__main__':
+    app.run(debug=True)
